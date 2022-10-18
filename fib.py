8fd05f7e24f9ff27e9b0a6cabbbe08313ea2d00e
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-10-18 14:45:05 -0700

    Update fib.py

--- fib.py
+++ fib.py
@@ -3,10 +3,10 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def fibin(num):
+def fib(num):
     number= ((((1 + (5) ** (0.5)) ** num) - (((1 - (5) ** (0.5)) ** num)))) // (((2) ** (num))*(5) ** (0.5))
     return number
 
-#print(fibin(9))
+#print(fib(9))
 
 
