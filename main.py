52bb50ab05ef896f9a137225ef47dfd8c090fa3a
klausmenendez <menendek@oregonstate.edu>
2022-10-18 14:41:04 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def fibin(num):
+    number= ((((1 + (5) ** (0.5)) ** num) - (((1 - (5) ** (0.5)) ** num)))) // (((2) ** (num))*(5) ** (0.5))
+    return number
+
+#print(fibin(9))
+
+
